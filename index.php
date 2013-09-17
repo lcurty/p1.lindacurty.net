ad64c739ead431deaabaafd059df4cfaef15270f
lcurty <lcurty@gmail.com>
2013-09-16 19:19:05 -0500

    Updates and additions
    Added content to index page, added headshot and styles.

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Welcome to P1</title>
-</head>
-
-<body>
-Welcome to P1
-</body>
+<!DOCTYPE HTML>
+<html>
+  <head>
+  	<meta charset="utf-8">
+		<title>Linda Curty Biography</title>
+	  <link href="styles/styles.css" rel="stylesheet" type="text/css" media="all">
+    <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+  	<div class="header">
+    	<div class="container">
+    		<h1>Linda Curty</h1>
+      </div>
+    </div>
+    <div class="main">
+    	<div class="container">
+        <img src="images/curty-headshot.png" alt="Linda Curty Headshot">
+        <hr class="clear">
+        <h2>Biography</h2>
+        <p>Linda Curty served as Systems Administrator/Web Developer at Jacobs' Technology Region home office in Tullahoma, Tennessee, since the summer of 2012. She joined the company in 2010 as an Inside Sales Coordinator. </p>
+        <p>Linda has a varied background in marketing communications that encompasses both for-profit and nonprofit experience. Prior positions have included Planned Giving Administrator at University of the South, Database Marketing Manager at United Way of Metropolitan Nashville, and Communications Specialist at PayMaxx Inc.</p>
+        <p>Additionally she has offered freelance photography and web site development services for more than five years.</p>
+        <p> Linda is a 2001 graduate of The University of Tennessee with a bachelor's degree in journalism and public relations, and she received a master's degree in mass communications from Middle Tennessee State University in 2007.</p>
+        <p>She currently resides in Winchester, Tennessee.</p>
+    	</div>
+    </div>
+    <footer><div class="container">&copy;2013 | <a href="mailto:lcurty@g.harvard.edu">Linda Curty</a></div></footer>
+  </body>
 </html>
