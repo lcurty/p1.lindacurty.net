7990a5f14799631ad19a4b41ae0236087aa9a7e5
lcurty <lcurty@gmail.com>
2013-09-16 19:52:56 -0500

    Added code editor, styled hr

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE HTML>
 <html>
   <head>
-  	<meta charset="utf-8">
+  	<meta charset="utf-8" />
 		<title>Linda Curty Biography</title>
-	  <link href="styles/styles.css" rel="stylesheet" type="text/css" media="all">
-    <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+	  <link href="styles/styles.css" rel="stylesheet" type="text/css" media="all" />
+    <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css' />
   </head>
   <body>
   	<div class="header">
@@ -14,16 +14,21 @@
     </div>
     <div class="main">
     	<div class="container">
-        <img src="images/curty-headshot.png" alt="Linda Curty Headshot">
-        <hr class="clear">
+        <img src="images/curty-headshot.png" alt="Linda Curty Headshot" />
+        <hr class="clear" />
         <h2>Biography</h2>
         <p>Linda Curty served as Systems Administrator/Web Developer at Jacobs' Technology Region home office in Tullahoma, Tennessee, since the summer of 2012. She joined the company in 2010 as an Inside Sales Coordinator. </p>
         <p>Linda has a varied background in marketing communications that encompasses both for-profit and nonprofit experience. Prior positions have included Planned Giving Administrator at University of the South, Database Marketing Manager at United Way of Metropolitan Nashville, and Communications Specialist at PayMaxx Inc.</p>
         <p>Additionally she has offered freelance photography and web site development services for more than five years.</p>
         <p> Linda is a 2001 graduate of The University of Tennessee with a bachelor's degree in journalism and public relations, and she received a master's degree in mass communications from Middle Tennessee State University in 2007.</p>
         <p>She currently resides in Winchester, Tennessee.</p>
+        <hr />
+        <h2>Code Editor</h2>
+        <p>I am using Adobe Dreamweaver CS5.5 as my code editor.</p>
     	</div>
     </div>
-    <footer><div class="container">&copy;2013 | <a href="mailto:lcurty@g.harvard.edu">Linda Curty</a></div></footer>
+  <footer><div class="container">
+      <p>&copy;2013 | <a href="mailto:lcurty@g.harvard.edu">Linda Curty</a></p>
+  </div></footer>
   </body>
 </html>
