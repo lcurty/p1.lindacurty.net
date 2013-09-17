907ebbb85bed524e541d976cfad0369999ed43b7
lcurty <lcurty@gmail.com>
2013-09-16 21:32:47 -0500

    grammar edit

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <img src="images/curty-headshot.png" alt="Linda Curty Headshot" />
         <hr class="clear" />
         <h2>Biography</h2>
-        <p>Linda Curty served as Systems Administrator/Web Developer at Jacobs' Technology Region home office in Tullahoma, Tennessee, since the summer of 2012. She joined the company in 2010 as an Inside Sales Coordinator. </p>
+        <p>Linda Curty has served as Systems Administrator/Web Developer at Jacobs' Technology Region home office in Tullahoma, Tennessee, since the summer of 2012. She joined the company in 2010 as an Inside Sales Coordinator. </p>
         <p>Linda has a varied background in marketing communications that encompasses both for-profit and nonprofit experience. Prior positions have included Planned Giving Administrator at University of the South, Database Marketing Manager at United Way of Metropolitan Nashville, and Communications Specialist at PayMaxx Inc.</p>
         <p>Additionally she has offered freelance photography and web site development services for more than five years.</p>
         <p> Linda is a 2001 graduate of The University of Tennessee with a bachelor's degree in journalism and public relations, and she received a master's degree in mass communications from Middle Tennessee State University in 2007.</p>
